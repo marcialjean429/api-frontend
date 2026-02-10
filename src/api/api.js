bd75dd09651ebde9a658e99fcf692559c17fda1b
marcialjean429 <marcialjean429@gmail.com>
2026-02-09 23:20:45 -0500

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -9,7 +9,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/users',
+    baseURL: 'https://spi-trannig.onrender.com',
     headers: {'Content-Type' : 'application/json'}
 });
 
